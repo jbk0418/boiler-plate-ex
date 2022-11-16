d81636b8a773e9a9500b59ab27fcdae24411cd2c
jbk0418 <wkdqudrnjsbg@naver.com>
2022-11-16 20:32:53 +0900

    회원가입 및 로그아웃

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -31,7 +31,7 @@ function LoginPage(props) {
       dispatch(loginUser(body))
         .then(response => {
           if(response.payload.loginSuccess){
-            navigate(-1);
+            navigate('/');
           }
           else{
             alert('Error"')
